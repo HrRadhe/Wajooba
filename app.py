b379eae3c4004f93e8e31374a14f36d47b04d331
Harshil Munjapara <munjaparah52@gmail.com>
2024-12-27 20:37:35 +0530

    database check

--- app.py
+++ app.py
@@ -1,31 +1,24 @@
 from huggingface_hub import InferenceClient
 import sqlite3
-import os
 
-# try:
-#     with sqlite3.connect("my.db") as conn:
-#         print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
-
-# except sqlite3.OperationalError as e:
-#     print("Failed to open database:", e)
-
-
-client = InferenceClient(api_key="note")
-
+# set llm   
 def llm_request(messages, model="microsoft/Phi-3.5-mini-instruct", maxtokens=500):
-    completion = client.chat.completions.create(
-        model=model, 
-        messages=messages, 
-        max_tokens=maxtokens
-    )
-    print(completion.choices[0].message)
-    return completion, completion.choices[0].message
+    # client = InferenceClient(api_key="note")
+    # completion = client.chat.completions.create(
+    #     model=model, 
+    #     messages=messages, 
+    #     max_tokens=maxtokens
+    # )
+    # print(completion.choices[0].message)
+    # return completion, completion.choices[0].message
+    return "", "result"
 
+# llm prompt
 def prompt(subject, audi_level, description):
     prompt = [
         {
             "role": "system",
-            "content": f"You are a teacher with hands-on experience in {subject}. Your tasks is to a create table of contents of course base on user small description. Be careful user need this course for a specific {audi_level} level of audience."
+            "content": f"You are a teacher with hands-on experience in {subject}. Your tasks is to a create table of contents of course base on user course description. Be careful user need this course for a specific {audi_level} level of audience."
         },
         {
             "role": "user",
@@ -34,7 +27,37 @@ def prompt(subject, audi_level, description):
     ]
     return prompt
 
+# create database
+def database():
+    connection = sqlite3.connect("test.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS course (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            subject TEXT,
+            level TEXT,
+            prompt TEXT,
+            output TEXT,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+        )
+    """)    
+    connection.commit()
+    return cursor, connection
 
+# save values in db
+def save_response(sub, level, prompt, output, cursor, conn):
+    cursor.execute("""
+            INSERT INTO course (subject, level, prompt, output)
+            VALUES (?, ?, ?, ?)
+        """, (sub, level, prompt, output))
+    conn.commit()
+    conn.close()
+
+# print response
+def show_response(response):
+    pass
+
+# user input
 try:
     # user inputs
     subject = input("Enter your subject: ")
@@ -49,9 +72,18 @@ try:
     
     # prompt for llm.
     llm_prompt = prompt(subject, audi_level, desc)
-    print(llm_prompt)
 
+    # course generation
+    withHeader, response = llm_request(llm_prompt)
+
+    # table creation
+    cursor, conn = database()
+
+    # store in db
+    save_response(subject, audi_level, desc, response, cursor, conn)
+# show input error
 except ValueError as ve:
     print(f"Input Error: {ve}")
+# show other error
 except Exception as e:
     print(f"Unexpected error: {e}")
